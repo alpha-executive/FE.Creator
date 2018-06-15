6d045876dd3b1a37374883b40043e5601e33aa08
zhangy44 <zhangy44@CNENZHANGY44L3C>
2018-06-15 18:06:06 +0800

    Add portal html code

---
+++ FE.Creator/FE.Creator.Admin/Areas/Portal/Controllers/PortalHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FE.Creator.Admin.Areas.Portal.Controllers
+{
+    public class PortalHomeController : Controller
+    {
+        // GET: Portal/PortalHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FE.Creator/FE.Creator.Admin/Areas/Portal/PortalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FE.Creator.Admin.Areas.Portal
+{
+    public class PortalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Portal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Portal_default",
+                "Portal/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
