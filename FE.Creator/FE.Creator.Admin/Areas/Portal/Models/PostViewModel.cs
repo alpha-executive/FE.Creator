f4be8799a3b6924045e89a70ca152b3fd0486988
alphateam <2wsx@WSX>
2018-07-16 22:31:00 +0800

    Add Portal Article Share

--- FE.Creator/FE.Creator.Admin/Areas/Portal/Models/PostViewModel.cs
+++ FE.Creator/FE.Creator.Admin/Areas/Portal/Models/PostViewModel.cs
@@ -7,6 +7,33 @@ namespace FE.Creator.Admin.Areas.Portal.Models
 {
     public class PostViewModel
     {
+        private string OriginalHost
+        {
+            get
+            {
+                Uri currentUri = HttpContext.Current.Request.Url;
+                string hostAndPort = string.Empty;
+                if (currentUri.OriginalString.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    hostAndPort = string.Format("http://{0}{1}", currentUri.Host,
+                        currentUri.Port == 80 || currentUri.Port == 443 ? string.Empty
+                        : ":" + currentUri.Port);
+                }
+
+                if (currentUri.OriginalString.StartsWith("https", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    hostAndPort = string.Format("https://{0}{1}", currentUri.Host,
+                       currentUri.Port == 80 || currentUri.Port == 443 ? string.Empty
+                       : ":" + currentUri.Port);
+                }
+
+                return hostAndPort;
+            }
+        }
+        public int ObjectId
+        {
+            get; set;
+        }
         public string PostTitle
         {
             get;set;
@@ -20,6 +47,33 @@ namespace FE.Creator.Admin.Areas.Portal.Models
         public string PostContent
         {
             get;set;
+        }
+
+        public string ContentUrl
+        {
+            get
+            {
+                return string.Format("{0}/portal/portalhome/viewarticlecontent/{1}",
+                    this.OriginalHost,
+                    this.ObjectId);
+            }
+        }
+
+
+        public string ImageUrl
+        {
+            get
+            {
+                if(this.ObjectId != 0)
+                {
+                   
+                    return string.Format("{0}/api/custom/SharedObjects/DownloadSharedBook/{1}?thumbinal=true",
+                       this.OriginalHost,
+                       this.ObjectId);
+                }
+
+                return string.Empty;
+            }
         }
 
         public string Author
